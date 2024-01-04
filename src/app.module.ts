7303ecbe1d0e24a987553fa9aae9abc7f3916c0d
Jonghyeon Choi <whd617@naver.com>
2024-01-04 15:07:31 +0900

    chore: TypeOrm 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModuleValidationSchema } from './configs/env-validation';
+import { typeOrmModuleOptions } from './configs/database.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: ConfigModuleValidationSchema,
+    }),
+    TypeOrmModule.forRootAsync(typeOrmModuleOptions),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
